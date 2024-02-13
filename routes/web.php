331f0a69803d6c1a39293747e70a7525e81502af
millatchowdhury <millatchowdhury@gmail.com>
2024-02-13 22:28:18 +0600

    front end and admin panel setup

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/', function(){
+    return view('website.home');
+});
+Route::get('/about', function(){
+    return view('website.about');
+});
+Route::get('/category', function(){
+    return view('website.category');
+});
+Route::get('/contact', function(){
+    return view('website.contact');
+});
+Route::get('/post', function(){
+    return view('website.post');
+});
+
+// admin panel routes 
+Route::get('/test', function(){
+    return view('admin.dashboard.index');
+});
